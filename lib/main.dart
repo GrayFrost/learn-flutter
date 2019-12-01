7424bd883831b25a572d913368cd16dbfafebe9c
zzh <1760655799@qq.com>
2019-12-01 21:56:06 +0800

    gridview widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/4_5_listview/listview_3.dart';
+import 'package:learn_flutter/4_5_listview/gridview.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       title: 'Hello World',
-      home: ListViewDemo3()
+      home: GridViewDemo()
     );
   }
 }
