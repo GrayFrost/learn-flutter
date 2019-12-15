248dd9008c12b49e1cc98b8a7ffb858eeb73fc2e
zzh <1760655799@qq.com>
2019-12-15 16:10:40 +0800

    feat(app): appbar action

--- lib/app.dart
+++ lib/app.dart
@@ -38,12 +38,51 @@ class AppState extends State<App> {
     }
   }
 
+  _popupMenuItem(String title, {String imagePath, IconData icon}) {
+    return PopupMenuItem(
+        child: Row(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: Text(title, style: TextStyle(color: Colors.white)),
+        )
+      ],
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         title: Text('即使通信'),
-        actions: <Widget>[],
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () {
+              //跳转至搜索页面
+              Navigator.pushNamed(context, 'search');
+            },
+            child: Icon(
+              //搜索图标
+              Icons.search,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 30.0, right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                showMenu(
+                    context: context,
+                    position: RelativeRect.fromLTRB(500.0, 76.0, 10.0, 0),
+                    items: <PopupMenuEntry>[
+                      _popupMenuItem('发起会话'),
+                      _popupMenuItem('添加好友'),
+                      _popupMenuItem('联系客服')
+                    ]);
+              },
+              child: Icon(Icons.add),
+            ),
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -54,18 +93,9 @@ class AppState extends State<App> {
           });
         }),
         items: [
-          BottomNavigationBarItem(
-            title: Text('聊天'),
-            icon: Icon(Icons.chat)
-          ),
-          BottomNavigationBarItem(
-            title: Text('好友'),
-            icon: Icon(Icons.person)
-          ),
-          BottomNavigationBarItem(
-            title: Text('我的'),
-            icon: Icon(Icons.home)
-          ),
+          BottomNavigationBarItem(title: Text('聊天'), icon: Icon(Icons.chat)),
+          BottomNavigationBarItem(title: Text('好友'), icon: Icon(Icons.person)),
+          BottomNavigationBarItem(title: Text('我的'), icon: Icon(Icons.home)),
         ],
       ),
       body: currentPage(),
